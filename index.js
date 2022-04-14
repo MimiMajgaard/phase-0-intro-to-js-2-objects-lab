cdabaf96f4347ef7790335d07a93ed47ec32b9b0
Mimi Majgaard <mimimajgaard@gmail.com>
2022-04-13 22:41:34 -0700

    commit tree 1880d4495073e15c2379ddd95870d6de921a6842

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-// Write your solution in this file!
+const employee = {
+    name: 'Sam',
+    streetAddress: '11 Broadway', 
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    const newEmployee = {...employee};
+    newEmployee[key] = value;
+    return newEmployee;
+
+}
+
+function destructivelyUpdateFromEmployeeByKey(employee, key, value){
+    employee.streetAddress = '12 Broadway'
+    employee[key] = value;
+    return employee;
+
+}
+    
+function deleteFromEmployeeByKey(employee, key, value){
+    delete employee.name; 
+    employee[key] = value;
+    return employee; 
+
+}
+
+
+function destructivelyDeleteFromEmployeeByKey(employee, key, value){
+    const newEmployee = destructivelyDeleteFromEmployeeByKey(employee, value);
+    
+}
+
+
+
+
+
+
+
+
+
+
+//spread operator: let objClone = { ...obj };
